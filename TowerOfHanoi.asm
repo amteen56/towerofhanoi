07302b40a6d23f6fb0a389640cc79a8824800c45
amteen56 <hellohunter99@gmail.com>
2018-12-10 21:33:12 +0500

    Version 1 TowerOfHanoi

--- TowerOfHanoi.asm
+++ TowerOfHanoi.asm
@@ -42,3 +42,84 @@ start:
     li 	$v0, 4;      
     la 	$a0, Exit;    			
     syscall;
+	
+	.data
+hanoi:
+	
+	slti $t1, $a0, 1
+	beq $t1, $t3, End	
+	
+	addi $sp, $sp, -20
+	sw $ra, 0($sp)		
+	sw $a0, 4($sp)		
+	sw $a1, 8($sp)		
+	sw $a2, 12($sp)		
+	sw $a3, 16($sp)		
+	
+	addi $a0, $a0, -1		
+	move $t2, $a2		
+	move $a2, $a3		
+	move $a3, $t2	
+	
+	jal hanoi		
+	
+	lw $ra, 0($sp)		
+	lw $a0, 4($sp)		
+	lw $a1, 8($sp)		
+	lw $a2, 12($sp)		
+	lw $a3, 16($sp)		
+	add $sp, $sp, 20
+		
+	beq $a1, $t3, FromA	
+	beq $a1, $t4, FromB	
+	beq $a1, $t5, FromC	
+	
+hanoi2:
+
+	addi $sp, $sp, -20
+	sw $ra, 0($sp)		
+	sw $a0, 4($sp)		
+	sw $a1, 8($sp)		
+	sw $a2, 12($sp)		
+	sw $a3, 16($sp)		
+	
+	move $t6, $a1		
+	move $t7, $a2		
+
+	move $a1, $a2		
+	move $a2, $t6		
+
+	addi $a0, $a0, -1	
+	jal hanoi		
+	
+	lw $ra, 0($sp)		
+	addi $sp, $sp, 20
+	jr $ra
+	
+FromA:
+	beq $a3, $t4, PrintAB	
+	beq $a3, $t5, PrintAC	
+	
+PrintAB:
+	addi $sp, $sp, -4
+	sw $a0, 0($sp)		
+	li  $v0, 4 
+	la  $a0, MoveAB
+	syscall
+	lw $a0, 0($sp)
+	addi $sp, $sp, 4
+	j hanoi2
+
+PrintAC:
+	addi $sp, $sp, -4
+	sw $a0, 0($sp)		
+	li  $v0, 4 
+	la  $a0, MoveAC
+	syscall
+	lw $a0, 0($sp)
+	addi $sp, $sp, 4
+	j hanoi2
+FromB:
+	beq $a3, $t3, PrintBA	
+	beq $a3, $t5, PrintBC	
+
